2f3b6c5ae456f1c09a97dc9fe3524b9d84474cec
FairyEver <1711467488@qq.com>
2019-09-03 14:02:27 +0800

    查看原始数据功能

---
+++ src/component/highlight/component.vue
@@ -0,0 +1,46 @@
+<style lang="scss" scoped>
+.highlight {
+  margin: 0px;
+  font-size: 12px;
+}
+</style>
+
+<template>
+  <pre class="highlight hljs" v-html="highlightHTML"></pre>
+</template>
+
+<script>
+// 相关文档
+// https://highlightjs.org/usage/
+// http://highlightjs.readthedocs.io/en/latest/api.html#configure-options
+import highlight from 'highlight.js'
+import './styles/github-gist.css'
+export default {
+  name: 'highlight',
+  props: {
+    value: {
+      type: String,
+      required: false,
+      default: ''
+    }
+  },
+  data () {
+    return {
+      highlightHTML: ''
+    }
+  },
+  mounted () {
+    this.highlight()
+  },
+  watch: {
+    value () {
+      this.highlight()
+    }
+  },
+  methods: {
+    highlight () {
+      this.highlightHTML = highlight.highlightAuto(this.value, [ 'json' ]).value
+    }
+  }
+}
+</script>
---
+++ src/views/result/raw/page.vue
@@ -0,0 +1,24 @@
+<template>
+  <container>
+    <highlight :value="code"/>
+    <div slot="footer" flex="main:center">
+      <copy :value="code"/>
+    </div>
+  </container>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+export default {
+  name: 'raw',
+  title: '原始数据',
+  computed: {
+    ...mapState([
+      'SCAN_RESULT'
+    ]),
+    code () {
+      return JSON.stringify(this.SCAN_RESULT, null, 2)
+    }
+  }
+}
+</script>
